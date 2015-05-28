1d2ab9ea5b014fb3a2373240ecd6700992b54811
Ben O'Halloran <ben@ohalloran.io>
2015-05-28 15:51:21 -0400

    format code

--- bop/dmmalloc.c
+++ bop/dmmalloc.c
@@ -9,11 +9,11 @@ If a PPR task runs out of memory, it must abort speculation. Calls to the underl
 The under study maintains access to the entire free list. Since either the understudy or the PPR tasks will survive past the commit stage, this is still safe.
 At commit time, the free lists of PPR tasks are merged along with the standard BOP changes. This allows memory not used by PPR tasks to be reclaimed and used later.
 Freeing
-when a PPR task frees something from the global heap (something it did not allocate, eg it was either allocated by a prior PPR task or before and PPRs were started) it marks as freed and moves to a new list. This list is parsed at commit time and is always (???) accepted. We cannot immediately move it into the free list since when allocating a new object of that size. If multiple PPR tasks do this (which is correct in sequential execution) and both allocate the new object, the merge will fail. 	
+when a PPR task frees something from the global heap (something it did not allocate, eg it was either allocated by a prior PPR task or before and PPRs were started) it marks as freed and moves to a new list. This list is parsed at commit time and is always (???) accepted. We cannot immediately move it into the free list since when allocating a new object of that size. If multiple PPR tasks do this (which is correct in sequential execution) and both allocate the new object, the merge will fail.
 
 Large objects:
 Size classes need to be finite, so there will be some sizes not handled by this method, the work around is:
-    allocation: if in PPR task, abort if not use DL malloc. 
+    allocation: if in PPR task, abort if not use DL malloc.
     free: when one of these is freed, check the block size. if it’s too large for any size class it was allocated with dl malloc. use dl free OR if sufficiently large divide up for use in our size classes.
 
 */
@@ -61,7 +61,7 @@ Size classes need to be finite, so there will be some sizes not handled by this 
 #define base_size ALIGNMENT //the smallest usable payload, anthing smaller than ALIGNMENT gets rounded up
 #define SIZE_C(k) (1 << (k + 5)) //allows for successive spliting
 
-typedef struct{
+typedef struct {
     header * start[NUM_CLASSES];
     header * end[NUM_CLASSES];
 } ppr_list;
@@ -74,7 +74,8 @@ header* headers[NUM_CLASSES];
 header* ends[NUM_CLASSES]; //end of lists in PPR region
 
 int  sizes[NUM_CLASSES] = {SIZE_C(1), SIZE_C(2), SIZE_C(3), SIZE_C(4),
-                           SIZE_C(5), SIZE_C(6), SIZE_C(7), SIZE_C(8)};
+                           SIZE_C(5), SIZE_C(6), SIZE_C(7), SIZE_C(8)
+                          };
 int counts[NUM_CLASSES];
 
 header* allocatedList = NULL;
@@ -83,17 +84,17 @@ header* freelist = NULL;
 int get_index(int size)
 {
     int index = 0;
-    //Space is too big. Return maximum possible index.                          
+    //Space is too big. Return maximum possible index.
     if (size >= (1<<(NUM_CLASSES-1)))
         return NUM_CLASSES-1;
-    //Divide by alignment until we have zero to get index.                            
+    //Divide by alignment until we have zero to get index.
     while (size/ALIGN(base_size + HSIZE)>0)
         index++;
     return index;
 }
 
 /** Divide up the currently allocated groups into regions*/
-void carve(int tasks){
+void carve(int tasks) {
     assert(tasks >= 2);
     regions = dm_malloc(tasks * sizeof(ppr_list));
     int index, count, j, r = 0;
@@ -102,15 +103,15 @@ void carve(int tasks){
     for(index = 0; index < NUM_CLASSES; index++)
         current_headers[index] = (header *) headers[index];
     //actually split the lists
-    for(index = 0; index < NUM_CLASSES; index++){
+    for(index = 0; index < NUM_CLASSES; index++) {
         count = counts[index] / tasks;
-        for(r = 0; r < tasks; r++){
+        for(r = 0; r < tasks; r++) {
             regions[r].start[index] = current_headers[index];
-            for(j = 0; j < count && temp; j++){
+            for(j = 0; j < count && temp; j++) {
                 temp = (header *) current_headers[index]->free.next;
             }
             current_headers[index] = temp;
-            if(r != tasks - 1){
+            if(r != tasks - 1) {
                 //the last task has no tail, use the same as seq. exectution
                 assert(temp != NULL);
                 regions[r].end[index] = (header *) temp->free.prev;
@@ -120,10 +121,10 @@ void carve(int tasks){
 }
 
 /**set the range of values to be used by this PPR task*/
-void initialize_group(int group_num){
+void initialize_group(int group_num) {
     ppr_list my_list = regions[group_num];
     int ind;
-    for(ind = 0; ind < NUM_CLASSES; ind++){
+    for(ind = 0; ind < NUM_CLASSES; ind++) {
         ends[ind] = my_list.end[ind];
         headers[ind] = my_list.start[ind];
     }
@@ -131,17 +132,17 @@ void initialize_group(int group_num){
 
 
 /**size: alligned size, includes space for the header*/
-static inline header * get_header(size_t size, int * which){
+static inline header * get_header(size_t size, int * which) {
     header * found = NULL;
     int ind;
     //requested allocation is too big
-    if(size > MAX_SIZE){
+    if(size > MAX_SIZE) {
         *which = -1;
         return NULL;
-    }else{
-    		*which = get_index(size);
-            found = headers[*which];
-         }
+    } else {
+        *which = get_index(size);
+        found = headers[*which];
+    }
     //clean up
     if(found == NULL || (/*TODO IN_PPR_TASK && */ CASTH(found) == ends[*which]->free.next))
         return NULL;
@@ -150,29 +151,29 @@ static inline header * get_header(size_t size, int * which){
 
 
 //actual malloc implementations
-void * dm_malloc(size_t size){
+void * dm_malloc(size_t size) {
     //get the right header
     size_t alloc_size = ALIGN(size + HSIZE);
     int which = -2;
     header * block = get_header(alloc_size, &which);
-    if(block == NULL){
+    if(block == NULL) {
         //no item in list. Either correct list is empty OR huge block
-        if(which == -1 /*&& !PPR*/){
+        if(which == -1 /*&& !PPR*/) {
             //use system malloc
             block = malloc(alloc_size);
             //don't need to add to free list, just set information
             block->allocated.blocksize = alloc_size; //huge, can check at free for the edge case
-        }else{
+        } else {
             //TODO BOP_ABORT() if in PPR
         }
         return block;
     }
     //update the new size class head
     headers[which] = (header *) block->free.next;
-    
+
     if(allocatedList == NULL)
         allocatedList = block;
-    else{
+    else {
         block->allocated.next = (void *) allocatedList;
         allocatedList = block;
     }
@@ -182,17 +183,17 @@ void * dm_malloc(size_t size){
     return (block + HSIZE);
 }
 
-void * dm_calloc(size_t n, size_t size){
+void * dm_calloc(size_t n, size_t size) {
     void * allocd  = dm_malloc(size * n);
     memset(allocd, 0, size * n);
     return allocd;
 }
 
-void * dm_realloc(void * ptr, size_t new_size){
+void * dm_realloc(void * ptr, size_t new_size) {
     void * payload = dm_malloc(new_size);
-    if(payload == NULL){
+    if(payload == NULL) {
         //failed to allocate new memory
-        return NULL;    
+        return NULL;
     }
     header * old_head = HEADER(ptr);
     size_t old_size = old_head->allocated.blocksize;
@@ -202,28 +203,28 @@ void * dm_realloc(void * ptr, size_t new_size){
     return payload;
 }
 
-void dm_free(void* ptr){
+void dm_free(void* ptr) {
     header * free_header = HEADER(ptr);
     int which = -1, size = free_header->allocated.blocksize;
     header * append_list;
-    if(0 /*TODO BOP_Running*/){
-        //just add to the free list   
+    if(0 /*TODO BOP_Running*/) {
+        //just add to the free list
         append_list = freelist;
-    }else{
-        if(size > MAX_SIZE){
+    } else {
+        if(size > MAX_SIZE) {
             free(free_header); //system free
             return;
         }
         append_list =  get_header(size, &which);
         //not in PPR mode, can immediately add to the free list
-        headers[which] = free_header;    
+        headers[which] = free_header;
         counts[which]++;
     }
     //append to the relevant list
-    if(append_list == NULL){
+    if(append_list == NULL) {
         *append_list = *free_header;
         free_header->free.next = free_header->free.prev = NULL; //one item in the list
-    }else{
+    } else {
         append_list->free.prev = CASTH(free_header);
         free_header->free.next = CASTH(append_list);
         append_list = free_header;
