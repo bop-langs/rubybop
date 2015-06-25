5a177515297816063b683d28adf78f1ef98114a9
Ben O'Halloran <bohallor@u.rochester.edu>
2015-06-25 14:57:46 -0400

    [malloc_wrapper] add headers for the unsupported operations

--- bop/malloc_wrapper.h
+++ bop/malloc_wrapper.h
@@ -5,9 +5,14 @@ void * malloc(size_t);
 void * realloc(void *, size_t);
 void free(void *);
 void * calloc(size_t, size_t);
-int posix_memalign(void**, size_t alignment, size_t size); //0 on success
+int posix_memalign(void**, size_t alignment, size_t size);
 void* aligned_malloc(size_t alignment, size_t size);
 size_t malloc_usable_size(void*);
+void* memalign(size_t size, size_t boundary);
+void* aligned_alloc(size_t size, size_t boundary);
+void* valloc(size_t size);
+struct mallinfo mallinfo();
+
 
 inline void * sys_malloc(size_t);
 inline void * sys_realloc(void *, size_t);
