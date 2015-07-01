c70baff284d729c9492d0272f7c88f7cce203439
Ben O'Halloran <ben@ohalloran.io>
2015-07-01 10:10:06 -0400

    [bop] Monitor processes based off of group ID and sleep between found children

--- bop/utils.h
+++ bop/utils.h
@@ -10,6 +10,7 @@ int get_int_from_env(const char* env, int min, int max, int def);
 void report_conflict( int verbose, mem_range_t *c1, char *n1,
 		      mem_range_t *c2, char *n2 );
 
+void nop(void);
 char mem_range_eq( mem_range_t *r1, mem_range_t *r2 );
 
 #include "external/malloc.h"
