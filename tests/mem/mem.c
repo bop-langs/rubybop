7fbd8df46c06b4fcf71456225aec75c12de0f620
ozyhuboi <yzhou22@u.rochester.edu>
2015-07-01 16:55:19 -0400

    mem tests work

--- tests/mem/mem.c
+++ tests/mem/mem.c
@@ -5,12 +5,12 @@
 
 /* The program makes an array of randomly initialized integers and adds them together. */
 
-#include "../../src/bop_api.h"
+#include "bop_api.h"
 
 int *dest;
 int sum;
 
-void initialize( int );
+//void initialize( int );
 void lots_of_allocation_on_block( int start, int end );
 
 int main(int argc, char ** argv)
@@ -41,27 +41,26 @@ int main(int argc, char ** argv)
  
   while ( data_size > 0 ) {
     int block_end = data_size;
+	data_size -= block_size;            
     int block_begin = data_size >= 0 ? data_size : 0 ;
-	data_size -= block_size;
+	
     
-	//BOP_ppr_begin(1);  /* Begin PPR */
+	BOP_ppr_begin(1);  /* Begin PPR */
 
 		lots_of_allocation_on_block(block_begin, block_end);
 
-    //BOP_ppr_end(1);  /* End PPR */
+    BOP_ppr_end(1);  /* End PPR */
   }
 	
 	for (i=0; i < size; i++) {
-		int diff = 1; 
-		//printf("dest: %d   i: %d \n", dest[i], i);	
-		//fflush(stdout); fflush(stdout);
+		int diff = 1; 	
 		
-		//diff = ((dest[i] - i) < .0001); 
-		if ( diff ) {
+		/*
+		if ( dest[i] != i ) {
 			printf("diff: %d; dest[i]: %d; i: %d \n", diff, dest[i], i);
 		}
-	
-		//assert (dest[i] == i);
+		*/
+		assert (dest[i] == i);
 	}
   //printf("%d: The sum is %.0f \n", getpid(),  );
   printf("end\n");
@@ -88,18 +87,12 @@ void initialize( int data_size ) {
 void lots_of_allocation_on_block( int start, int end ) {
   int j;
   int *addr; 
-  /*
-  double total = 0;
-  for ( j = start ; j < end ; j ++ ) 
-    total += sin(data[ j ]) * sin(data[ j ]) + cos(data[ j ]) * cos(data[ j ]);
-
-  BOP_use( &data[j], sizeof( double )*(end - start) );
-  */
+ 
   for (j = start; j < end; j++) {
-			addr = (int*)malloc(sizeof(int)); 
+			addr = malloc(sizeof(int)); 
 			*addr = j; 
 			dest[j] = *addr; 
-			
+			//printf("dest[j]: %d and addr: %d\n", dest[j], *addr);			
 			BOP_promise( &dest[j], sizeof(int) ); 
   } 		
  
