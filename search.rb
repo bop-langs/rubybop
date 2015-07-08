bc5f1542beeb00de0a583bf1bcc052c2d6b57a34
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-08 17:22:43 -0400

    added some fixes to search.rb, basically just making it more strict in where it gets its args, and added support for it in rakefile

--- search.rb
+++ search.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/ruby
 require 'find'
 
-$pre_terms = ["VALUE"]
+$pre_terms = ["VALUE", "static", "void"]
 $search_terms = [/\(.*VALUE.*\)/, /\(.*\)/, /\{/]
 $next_terms = ["{"]
-$bad_terms = ["//", "for", "if", "while", "switch", /\t/ , "\\", "*"]
+$bad_terms = ["//", "for", "if", "while", "switch", /\t/ , "\\", "*", ";"]
 $bad_pre_terms = $bad_terms << "{}"
 $bad_next_terms = $bad_terms
 
@@ -22,32 +22,43 @@ def search(terms, line)
   return i
 end
 
+class String
+  def string_between_markers marker1, marker2
+    self[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
+  end
+end
+
 def find_args (line)
   vals = Array.new()
-  words = line.split(/\W+/)
+  args = line.string_between_markers("(", ")")
+  words = args.split(/\W+/)
   words.each_with_index do |word, word_no|
     if word == "VALUE"
-      vals << words[word_no+1]
+      unless words[word_no+1] == "const"
+        vals << words[word_no+1]
+      else
+        vals << words[word_no+2]
+      end
     end
   end
   return vals
 end
 
 def parse_file(file_name)
-  has_bop_api = false
-  first_arg = 0
+  #has_internal = false
+  #first_arg = 0
   file = File.readlines(file_name)
   file.each_with_index do |line, line_no|
-    unless line.scan("bop_api.h").empty?
-      has_bop_api = true
-    end
+    #unless line.scan("internal.h").empty?
+    #  has_internal = true
+    #end
     prev_line = file[line_no -1]
     next_line = file[line_no +1]
     i = 0
     i += 5*search($search_terms, line)
     i -= 10*search($bad_terms, line)
     unless line_no == 0
-      i += 2*search($pre_terms, prev_line)
+      i += 1*search($pre_terms, prev_line)
       i -= 10*search($bad_pre_terms, prev_line)
     end
     unless file[line_no+1].nil?
@@ -57,22 +68,20 @@ def parse_file(file_name)
 
     if i >= $cutoff
       args = find_args(line)
-      puts "score = #{i}"
-      puts "#{file_name}:\t #{line_no-1}: #{prev_line}"
-      puts "#{file_name}:\t #{line_no}: #{line}"
-      puts "#{file_name}:\t #{line_no+1}: #{next_line}"
-      puts
+
       args.each do |arg| file.insert(line_no+2, "BOP_obj_use_promise(#{arg});") end
-      puts
-      if first_arg == 0
-        first_arg = line_no
-      end
+
+      #if first_arg == 0
+      #  first_arg = line_no
+      #end
     end
   end
-  unless has_bop_api
-    file.insert(first_arg - 2, "#include bop_api.h")
-  end
-  if File.exists?(file_name<<".test")
+  #unless has_internal
+    #if first_arg > 0
+      #file.insert(first_arg - 1, "#include \"internal.h\"")
+    #end
+  #end
+  if File.exists?(file_name)
     File.delete(file_name)
   end
   File.open(file_name, mode="w+") do |f|
