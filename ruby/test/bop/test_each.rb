1356a141e82129f00c67e8d765bff4dee5e1e720
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-16 14:39:25 -0400

    actually added test each

---
+++ ruby/test/bop/test_each.rb
@@ -0,0 +1,30 @@
+puts "start"
+a = 1
+b = 2
+c = 3
+d = 4
+puts a
+
+PPR.yield{
+  sleep(1); a=2; a = "FIRE" ; PPR.promise(a)
+  }
+PPR.yield{
+  sleep(1); puts b; b="good2"; PPR.promise(b)
+  }
+PPR.yield{
+  sleep(1); puts c; c="goodgood3"; PPR.promise(c)
+  }
+PPR.yield{
+  sleep(1); puts d; d="goodgoodgood4"; PPR.promise(d)
+  }
+
+
+
+
+
+puts a
+puts b
+puts c
+puts d
+
+puts "finish"
