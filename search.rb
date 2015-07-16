e4c4c5a86bd0e034594a8b25eab567211d93cbad
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-16 14:41:23 -0400

    made it so BOP will never be called on flags, may need to revert this later but for now is a sanity check

--- search.rb
+++ search.rb
@@ -8,11 +8,13 @@ $bad_terms = ["//", "for", "if", "while", "switch", /\t/ , "\\", "*", ";"]
 $bad_pre_terms = $bad_terms << "{}"
 $bad_next_terms = $bad_terms
 
+$black_objs = ["flags"]
+
 #$accepted_files = [/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/]
 $accepted_files = [/\.c\Z/]
 #$accepted_files = ["main.c","thread.c", "loadpath.c", "version.c", "localeinit.c", "dln.c","dmyext.c","dmyext.c", "compile.c", "load.c", "dln_find.c", "goruby.c","eval_jump.c", "enum.c", "enumerator.c","inits.c", "file.c","iseq.c", "pack.c", "safe.c", "marshal.c","random.c", "sparc.c","array.c", "bignum.c", "addr2line.c","error.c", "parse.c", "st.c", "re.c", "node.c", "debug.c"]
 $ignored_files = ["miniinit.c", "compar.c", "ppr.c", "ppr_mon.c",
-  "ruby.c", "eval.c", "gc.c", "io.c", "dir.c", "hash.c", "st.c",
+  #"ruby.c", "eval.c", "gc.c", "io.c", "dir.c", "hash.c", "st.c",
   /vm/, /ext/,  /enc/, /missing/, /win32/, /nacl/]
 
 $cutoff = 14
@@ -41,10 +43,14 @@ def find_args (line)
   words = args.split(/\W+/)
   words.each_with_index do |word, word_no|
     if word == "VALUE"
+      arg = ""
       unless words[word_no+1] == "const"
-        vals << words[word_no+1]
+        arg = words[word_no+1]
       else
-        vals << words[word_no+2]
+        arg =  words[word_no+2]
+      end
+      unless search($black_objs, arg) > 0
+        vals << arg
       end
     end
   end
