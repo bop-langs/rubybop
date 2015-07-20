56328440e142dc6244a4ceea4342c3857c2a4c87
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-20 14:33:58 -0400

    [test_each_nobop.rb] made it match current test and actually work

--- ruby/test/bop/test_each_nobop.rb
+++ ruby/test/bop/test_each_nobop.rb
@@ -1,34 +1,24 @@
 puts "start"
-a = 1
-b = 2
-c = 3
-d = 4
-puts a
+$a = "1"
+$b = "2"
+$c = "3"
+$d = "4"
 
-#PPR.yield
-#  {
-  sleep(1); a=2; a = "FIRE" ; PPR.promise(a)
-#  }
-#PPR.yield
-#  {
-  sleep(1); puts b; b="good2"; PPR.promise(b)
-#  }
-#PPR.yield
-#  {
-  sleep(1); puts c; c="goodgood3"; PPR.promise(c)
-#  }
-#PPR.yield
-#  {
-  sleep(1); puts d; d="goodgoodgood4"; PPR.promise(d)
-#  }
+  $a="good"
+  sleep(1)
 
+  $b="good"
+  sleep(1)
 
+  $c="good"
+  sleep(1)
 
+  $d="good"
+  sleep(1);
 
-
-puts a
-puts b
-puts c
-puts d
+puts $a
+puts $b
+puts $c
+puts $d
 
 puts "finish"
