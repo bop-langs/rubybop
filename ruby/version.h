ebaeab201e56ee252bac1fb5ae372fadc0f6a6ae
Ben O'Halloran <ben@ohalloran.io>
2015-07-22 17:55:54 -0400

    Update version info for ruby bop

--- ruby/version.h
+++ ruby/version.h
@@ -1,4 +1,4 @@
-#define RUBY_VERSION "2.2.3 bop"
+#define RUBY_VERSION "2.2.3"
 #define RUBY_RELEASE_DATE "2015-05-21"
 #define RUBY_PATCHLEVEL 115
 
@@ -40,7 +40,7 @@
 #endif
 
 # define RUBY_DESCRIPTION	    \
-    "ruby "RUBY_VERSION		    \
+    "rubybop "RUBY_VERSION		    \
     RUBY_PATCHLEVEL_STR		    \
     " ("RUBY_RELEASE_DATE	    \
     RUBY_REVISION_STR") "	    \
