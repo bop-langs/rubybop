56f52be2d35c5bc8b08bc6a19fb15782d4c71f7d
Ben O'Halloran <ben@ohalloran.io>
2015-07-23 16:39:10 -0400

    [gems] Remove debug info

--- ruby/gem_prelude.rb
+++ ruby/gem_prelude.rb
@@ -2,6 +2,4 @@ puts $LOAD_PATH
 $LOAD_PATH.map! { |var|
   var = var.gsub('ruby', 'rubybop') if ! var.include?('rubybop')
 }
-puts "new loadpath"
-puts $LOAD_PATH
 require 'rubygems.rb' if defined?(Gem)
