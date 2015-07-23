3346cb82071dede1e449a73b01c82bcb1bdcdede
Ben O'Halloran <ben@ohalloran.io>
2015-07-23 16:27:59 -0400

    ruby -> rubybop. gems work via ruby->rubybop at launch

--- ruby/gem_prelude.rb
+++ ruby/gem_prelude.rb
@@ -1,1 +1,7 @@
+puts $LOAD_PATH
+$LOAD_PATH.map! { |var|
+  var = var.gsub('ruby', 'rubybop') if ! var.include?('rubybop')
+}
+puts "new loadpath"
+puts $LOAD_PATH
 require 'rubygems.rb' if defined?(Gem)
