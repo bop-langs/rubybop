ea2b75a0d730eae7968e7a8d117f57361b5616d4
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-27 16:12:52 -0400

    added a test that needs test_newobj.rb

---
+++ ruby/test/bop/test_newobj.rb
@@ -0,0 +1,25 @@
+puts "start"
+
+PPR {
+  $a="good"
+  sleep(1)
+  }
+PPR {
+  $b="good"
+  sleep(1)
+  }
+PPR {
+  $c="good"
+  sleep(1)
+  }
+PPR {
+  $d="good"
+  sleep(1);
+  }
+
+puts $a
+puts $b
+puts $c
+puts $d
+
+puts "finish"
