bf915e602531e4d83fe2613acf155627982b512a
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-07-27 15:01:49 -0400

    moved tests from rubybop to master

--- ruby/bootstraptest/test_bop.rb
+++ ruby/bootstraptest/test_bop.rb
@@ -26,5 +26,24 @@ unless($a+$b+$c+$d == "goodgoodgoodgood")
 end
 
 }
+assert_finish 4 ,%q{
+$a = [0, 0, 0, 0]
+PPR {
+	sleep(1)
+	$a[0] = 1
+}
+PPR {
+	sleep(1)
+	$a[1] = 2
+}
+PPR {
+	sleep(1)
+	$a[2] = 3
+}
+PPR {
+	sleep(1)
+	$a[3] = 4
+}
 
-
+$a == [1,2,3,4]
+}
