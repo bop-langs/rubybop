e9fc4b657819d85b960f901c03e47cae9858b41a
Ben O'Halloran <bohallor@u.rochester.edu>
2015-08-07 10:50:58 -0400

    Rewrite dmmalloc's grow function, possible fix for same head allocations

--- bop/malloc_wrapper.c
+++ bop/malloc_wrapper.c
@@ -1,5 +1,4 @@
 //NOTE: do not compile with optimizations on. The hack to get calloc working (used in glibc/dlsym) is relies on undefined behavior, but has been tested to work correctly on redhat, fedora and ubuntu using GCC, and ubuntu with CLANG as well
-
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
