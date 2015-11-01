138642aa8fd2210c52a0adeb634dd2169772e09a
Jacob Bisnett <jacob.bisnett@gmail.com>
2015-10-31 21:46:34 -0400

    added primes test (largest prime divisor)

---
+++ ruby/test/bop/primes_nobop.rb
@@ -0,0 +1,28 @@
+require "prime"
+$input = ARGF.map{|i| i.to_i}
+#$input = (100000000..100000020).to_a
+$output_array = Array.new $input.size, 0
+$sum = 0
+
+$input.each_with_index do |i, j|
+    if i == 1
+        $output_array[j]= 1
+        next
+    end
+    if Prime.prime?(i)
+        $output_array[j]= i
+        next
+    end
+    t = i/2
+    t.times do |x|
+        if i.gcd(x) == x
+            if Prime.prime?(i/x)
+               $output_array[j]= i/x
+               break
+            end
+        end
+    end
+end
+
+PPR.over
+puts $output_array
