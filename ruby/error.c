607a383aa749a05b331abe36320c33b19c00da7f
Ben O'Halloran <bohallor@u.rochester.edu>
2016-04-30 18:19:26 -0400

    Update the RubyBOP repo URL
    [ci skip]

--- ruby/error.c
+++ ruby/error.c
@@ -52,7 +52,7 @@ static const char REPORTBUG_MSG[] =
 #if defined __APPLE__
 	"Don't forget to include the above Crash Report log file.\n"
 #endif
-	"Please submit issues to the Rubybops repository, http://www.github.com/dcompiler/rubybop \n\n" \
+	"Please submit issues to the Rubybop repository, http://www.github.com/bop-langs/rubybop \n\n" \
     ;
 
 static const char *
